1a4912f20dec5d427a07bfa49cba5ca78a0e7714
hugh.li <hugh.li@foxmail.com>
2021-03-30 16:42:26 +0800

    feat: 修改alifun的名称

--- fun.rb
+++ fun.rb
@@ -1,4 +1,4 @@
-class alifun < Formula
+class Fun < Formula
   desc "Fun is a development tool for serverless applications of aliyun."
   homepage "https://github.com/alibaba/funcraft"
   version "3.6.21"
@@ -7,7 +7,7 @@ class alifun < Formula
 
 
   def install
-    bin.install "fun-v#{version}-macos" => "alifun"
+    bin.install "fun-v#{version}-macos" => "fun"
   end
 
   test do
