587c16cc7229bb39641ba141b7a3e9e5c738be99
hugh.li <hugh.li@foxmail.com>
2021-03-30 11:13:24 +0800

    feat: 添加阿里云fun的安装

---
+++ ali-fun.rb
@@ -0,0 +1,16 @@
+class AliFun < Formula
+  desc "Fun is a development tool for serverless applications of aliyun."
+  homepage "https://github.com/alibaba/funcraft"
+  version "3.6.21"
+  url "https://gosspublic.alicdn.com/fun/fun-v#{version}-macos.zip"
+  sha256 "d88ba24d9f1f812fc7c69ec90cffc51a411f3a4368da92bf3101505cca97d90b"
+
+
+  def install
+    bin.install "fun-v#{version}-macos" => "ali-fun"
+  end
+
+  test do
+    system "false"
+  end
+end
