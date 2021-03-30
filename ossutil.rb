d38941c0ddc7a20174dec6dc46a2c113cc87500c
hugh.li <hugh.li@foxmail.com>
2021-03-30 16:53:31 +0800

    feat: 添加ossutil

--- ossutil.rb
+++ ossutil.rb
@@ -1,7 +1,7 @@
 class Ossutil < Formula
     desc "The command line interface of oss of aliyun."
     homepage "https://github.com/aliyun/ossutil"
-    version "1.7.2"
+    version "1.7.1"
     url "http://gosspublic.alicdn.com/ossutil/#{version}/ossutilmac64"
     sha256 "cdab34aa7e46fe4f4398e7abac1efb24ce44876af4bb9d240cf1caebff05062c"
 
