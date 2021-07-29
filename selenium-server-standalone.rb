df86ffd3061af801ebaacc5c28576ac1f22cafb0
hugh.li <hugh.li@foxmail.com>
2021-07-29 23:27:13 +0800

    feat: selenium-server-standalone 4.0

--- selenium-server-standalone.rb
+++ selenium-server-standalone.rb
@@ -4,52 +4,16 @@ class SeleniumServerStandalone < Formula
   url "https://selenium-release.storage.googleapis.com/4.0-beta-4/selenium-server-4.0.0-beta-4.jar"
   sha256 "4cbd0f5dfa425e0e11c466c45cfd503d9249e8fceb0e425f3adab849655437e0"
   license "Apache-2.0"
-  revision 2
-
-  livecheck do
-    url "https://www.selenium.dev/downloads/"
-    regex(/href=.*?selenium-server-standalone[._-]v?(\d+(?:\.\d+)+)\.jar/i)
-  end
+  vision "4.0.0-beta-4"
 
   depends_on "openjdk"
 
   def install
     libexec.install "selenium-server-standalone-#{version}.jar"
-    bin.write_jar_script libexec/"selenium-server-standalone-#{version}.jar", "selenium-server"
+    bin.write_jar_script libexec/"selenium-server-#{version}.jar", "selenium-server"
   end
 
   plist_options manual: "selenium-server -port 4444"
-
-  def plist
-    <<~EOS
-      <?xml version="1.0" encoding="UTF-8"?>
-      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-      <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <false/>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{Formula["openjdk"].opt_bin}/java</string>
-          <string>-jar</string>
-          <string>#{libexec}/selenium-server-standalone-#{version}.jar</string>
-          <string>-port</string>
-          <string>4444</string>
-        </array>
-        <key>ServiceDescription</key>
-        <string>Selenium Server</string>
-        <key>StandardErrorPath</key>
-        <string>#{var}/log/selenium-error.log</string>
-        <key>StandardOutPath</key>
-        <string>#{var}/log/selenium-output.log</string>
-      </dict>
-      </plist>
-    EOS
-  end
 
   test do
     port = free_port
