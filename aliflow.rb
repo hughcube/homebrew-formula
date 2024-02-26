6a65c370a663e756d257c8b012a0f9b7588a2afb
hugh.li <hugh.li@foxmail.com>
2024-02-26 16:36:43 +0800

    feat: aliflow 升级

--- aliflow.rb
+++ aliflow.rb
@@ -1,9 +1,9 @@
 class Aliflow < Formula
   desc "Flow is a development tool for serverless applications of aliyun."
-  homepage "https://help.aliyun.com/document_detail/153811.html?spm=a2cl9.flow_devops2020_goldlog_detail.0.0.6d40680f3OHhIJ"
-  version "0.0.6"
-  url "https://rdc-plugin-storage.oss-cn-beijing.aliyuncs.com/flow-cli/#{version}/aliyun/flow-step-cli_darwin_amd64"
-  sha256 "da0edc069421a3db05b137402db49a4ed8a5acf40039c0941dd3a7649af66ea2"
+  homepage "https://help.aliyun.com/document_detail/153811.html"
+  version "1.0"
+  url "https://rdc-plugin-storage.oss-cn-beijing.aliyuncs.com/flow-cli/#{version}/darwin_arm64_flow_cli"
+  sha256 "9cc90f52a7b3aa24d3016e5142127098e658e5809e4d9e9a0afb2602a067b996"
 
 
   def install
